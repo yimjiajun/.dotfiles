5cea3cbdb478cf65ee221f5815a2ef049928ec06
JiaJun Yim <yimjiajun@icloud.com>
2023-06-26 18:37:02 -0700

    feat tool: htop
    purpose: review os system usage

---
+++ tools/htop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+install() {
+	local install=$(./get_intall_pkg_cmd.sh)
+	echo $install "$?"
+	if [[ -z $install ]] ||\
+		[[ $install =~ ^Error* ]] ||\
+		[[ $install =~ ^error* ]] ||\
+		[[ $install =~ ^err:* ]]; then
+		echo -e "\033[31mError: install package not found ! \033[0m" >&2
+		exit 1
+	fi
+
+	./common.sh display_tittle "Install htop"
+	$install htop
+}
+
+if [[ $OSTYPE != linux-gnu* ]]; then
+	echo -e "\033[31mError: htop only for Linux\033[0m"
+	exit 1
+fi
+
+if [[ -z "$(which htop)" ]] ||\
+	[[ $1 == "install" ]]; then
+	install
+fi
+
+exit 0
