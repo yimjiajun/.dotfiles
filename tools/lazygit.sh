547f1a91b16eca01b9c6eb81421411d32563a277
JiaJun Yim <yimjiajun@icloud.com>
2023-06-26 21:53:33 -0700

    feat tool: lazygit
    gui git interface

---
+++ tools/lazygit.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+install() {
+	./common.sh display_tittle "Install LazyGit"
+
+	if [[ $OSTYPE == linux-gnu* ]]; then
+		. /etc/os-release
+
+		if [[ $ID == 'ubuntu' ]]; then
+			cd /tmp/
+			LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+			curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+			tar xf lazygit.tar.gz lazygit
+			sudo install lazygit /usr/local/bin
+
+			if [[ $? -ne 0 ]] || [[ -z $(which lazygit) ]]; then
+				echo -e "\033[31mError: install lazygit failed ! \033[0m" >&2
+				exit 1
+			else
+				exit 0
+			fi
+		fi
+	fi
+
+	local install=$(./get_intall_pkg_cmd.sh)
+
+	if [[ -z $install ]] ||\
+		[[ $install =~ ^Error* ]] ||\
+		[[ $install =~ ^error* ]] ||\
+		[[ $install =~ ^err:* ]]; then
+		echo -e "\033[31mError: install package not found ! \033[0m" >&2
+		exit 1
+	fi
+
+	$install lazygit
+
+	if [[ $? -ne 0 ]]; then
+		echo -e "\033[31mError: install lazygit failed ! \033[0m" >&2
+		exit 1
+	fi
+}
+
+if [[ -z "$(which lazygit)" ]] ||\
+	[[ $1 == "install" ]]; then
+	install
+fi
+
+exit 0
