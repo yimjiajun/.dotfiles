bff215ca67c5aa9a935178b2c257bbf6c77154b9
JiaJun Yim <yimjiajun@icloud.com>
2023-06-26 21:13:42 -0700

    feat tool: ranger
    purpose: gui file manager

---
+++ tools/ranger.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+install() {
+	local install=$(./get_intall_pkg_cmd.sh)
+
+	if [[ -z $install ]] ||\
+		[[ $install =~ ^Error* ]] ||\
+		[[ $install =~ ^error* ]] ||\
+		[[ $install =~ ^err:* ]]; then
+		echo -e "\033[31mError: install package not found ! \033[0m" >&2
+		exit 1
+	fi
+
+	./common.sh display_tittle "Install ranger"
+	$install ranger
+
+	if [[ $? -ne 0 ]]; then
+		echo -e "\033[31mError: install ranger failed ! \033[0m" >&2
+		exit 1
+	fi
+}
+
+if [[ -z "$(which ranger)" ]] ||\
+	[[ $1 == "install" ]]; then
+	install
+fi
+
+exit 0
