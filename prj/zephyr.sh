ec0f45c629c5fc6c81a058a05497854222483873
JiaJun Yim <yimjiajun@icloud.com>
2023-07-14 00:09:51 -0700

    feat prj/zephyr.sh: zephyr project
    reference: https://docs.zephyrproject.org/latest/develop/getting_started/index.html

---
+++ prj/zephyr.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+path=$(dirname $(readlink -f $0))
+zephyr_sdk_version="0.16.1"
+
+install() {
+	local tmp_dir=$(mktemp -d)
+	local zephyr_dir="$HOME/zephyrproject"
+
+	$path/common.sh display_tittle "Zephyr Project"
+
+	wget https://apt.kitware.com/kitware-archive.sh -O $tmp_dir/kitware-archive.sh || {
+		echo -e "\e[31mError: Failed to download kitware-archive.sh\e[0m"
+		exit 1
+	}
+
+	sudo bash $tmp_dir/kitware-archive.sh || {
+		echo -e "\e[31mError: Failed to add the Kitware APT repository to your sources list\e[0m"
+		exit 1
+	}
+
+	sudo apt install -y --no-install-recommends git cmake ninja-build gperf \
+	  ccache dfu-util device-tree-compiler wget \
+	  python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file \
+	  make gcc gcc-multilib g++-multilib libsdl2-dev libmagic1 1>/dev/null || {
+
+	  	ehco -e "\e[31mError: Failed to install dependencies\e[0m"
+	}
+
+	pip3 install --user -U west || {
+		echo -e "\e[31mError: Failed to install west\e[0m"
+		exit 1
+	}
+
+	if [[ $(grep -c "export PATH=~/.local/bin:$PATH" ~/.bashrc) -eq 0 ]]; then
+		echo 'export PATH=~/.local/bin:"$PATH"' >> ~/.bashrc
+		source ~/.bashrc
+	fi
+
+	west init $zephyr_dir 2>/dev/null || {
+		if ! [[ -f $zephyr_dir/.west/config ]]; then
+			if [[ -d $zephyr_dir/.west ]]; then
+				rm -rf $zephyr_dir/.west
+			fi
+
+			west init $zephyr_dir || {
+				echo -e "\e[31mError: Failed to initialize west\e[0m"
+				exit 1
+			}
+		fi
+	}
+
+	cd $zephyr_dir
+	west update --narrow &
+
+	west zephyr-export || {
+		echo -e "\e[31mError: Failed to Export a Zephyr CMake package\e[0m"
+		exit 1
+	}
+
+	pip3 install --user -r ~/zephyrproject/zephyr/scripts/requirements.txt 1>/dev/null || {
+		echo -e "\e[31mError: Failed to install Python dependencies\e[0m"
+		exit 1
+	}
+
+	cd ~
+
+	if ! [[ -d zephyr-sdk-${zephyr_sdk_version} ]]; then
+		if ! [[ -f zephyr-sdk-${zephyr_sdk_version}_linux-x86_64.tar.xz ]]; then
+			wget https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${zephyr_sdk_version}/zephyr-sdk-${zephyr_sdk_version}_linux-x86_64.tar.xz || {
+				echo -e "\e[31mError: Failed to download zephyr-sdk-${zephyr_sdk_version}_linux-x86_64.tar.xz\e[0m"
+				exit 1
+			}
+
+			wget -O - https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${zephyr_sdk_version}/sha256.sum | shasum --check --ignore-missing || {
+				echo -e "\e[31mError: Failed to verify zephyr-sdk-${zephyr_sdk_version}_linux-x86_64.tar.xz\e[0m"
+				exit 1
+			}
+
+			tar xf zephyr-sdk-${zephyr_sdk_version}_linux-x86_64.tar.xz || {
+				echo -e "\e[31mError: Failed to extract zephyr-sdk-${zephyr_sdk_version}_linux-x86_64.tar.xz\e[0m"
+				exit 1
+			}
+
+			rm zephyr-sdk-${zephyr_sdk_version}_linux-x86_64.tar.xz
+		fi
+
+		echo -e "setup zephyr-sdk-${zephyr_sdk_version} ..."
+		cd zephyr-sdk-${zephyr_sdk_version}
+		./setup.sh -t 'all' || {
+			echo -e "\e[31mError: Failed to setup zephyr-sdk-${zephyr_sdk_version}\e[0m"
+			exit 1
+		}
+	fi
+}
+
+if [[ -z $(which west) ]] || \
+	[[ $1 == 'install' ]]; then
+	install
+fi
+
+exit 0
