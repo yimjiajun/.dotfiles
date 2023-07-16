6be696d1f6291df5a3b93026dfb02b75b21bcd4f
JiaJun Yim <yimjiajun@icloud.com>
2023-07-15 20:57:30 -0700

    fix app: move browser into network

--- app/network.sh
+++ app/network.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 tput clear
 
-func=('device_ip')
+func=('device_ip' 'open_url')
 func=($(printf '%s\n' "${func[@]}"|sort))
 
 common="$(dirname $(readlink -f "$0"))/common.sh"
@@ -13,6 +13,40 @@ dev_ip="$(ip addr show | grep -E 'inet.*brd' | awk '{print $2}' | cut -d '/' -f 
 device_ip() {
 	$common display_subtitle "device ip"
 	$common display_info "IP address" "$dev_ip"
+}
+
+open_url() {
+	$common display_subtitle "OPEN URL"
+	$common display_info "Enter URL: (https://)"; read url
+
+	if ! [[ "$url" =~ ^https?:// ]]; then
+		url="http://$url"
+	fi
+
+	$common display_info "URL" "$url"
+
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		open "$url"
+		return
+	fi
+
+	if [[ "$OSTYPE" == "cygwin" ]]; then
+		cygstart "$url"
+		return
+	fi
+
+	if [[ -d /run/WSL ]]; then
+		wslview "$url"
+		return
+	fi
+
+	if [[ -x "$(command -v xdg-open)" ]]; then
+		xdg-open "$url"
+		return
+	fi
+
+	$common display_error "no browser tool found to open !"
+	exit 1
 }
 
 select option in "${func[@]}"; do
