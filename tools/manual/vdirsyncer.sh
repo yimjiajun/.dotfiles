5d58e6e42057fffc29e26103738f2fc502da27cb
JiaJun Yim <yimjiajun@icloud.com>
2023-07-19 02:00:24 -0700

    feat tools/manual: vdirsyncer install

--- tools/manual/vdirsyncer.sh
+++ tools/manual/vdirsyncer.sh
@@ -1,46 +1,63 @@
 #!/bin/bash
 
+tool="vdirsyncer"
 path=$(dirname $(readlink -f $0))
 common="$path/../../app/common.sh"
+data_path="$(dirname $(readlink -f $0))/../../.localdata/vdirsyncer"
 
 install() {
-	local data_path="$(dirname $(readlink -f $0))/../../.localdata/vdirsyncer"
-	local local_data_path="$HOME/.config/vdirsyncer"
+	local local_data_path="${HOME}/.config/vdirsyncer"
 	local discover_module_name="personal_sync"
 	local install="pip3 install --upgrade-strategy eager"
 
 	$common display_title "Install vdirsyncer"
-	$install vdirsyncer
 
-	if [[ $? -ne 0 ]]; then
-		echo -e "\033[31mError: install vdirsyncer failed ! \033[0m" >&2
+	$install $tool || {
+		$common display_error "Install vdirsyncer failed !"
 		exit 1
-	fi
+	}
+
+	$install aiohttp-oauthlib || {
+		pip3 install --upgrade-strategy eager aiohttp-oauthlib || {
+			$common display_error "Install aiohttp-oauthlib failed !"
+			exit 1
+		}
+	}
 
 	if ! [[ -f ${data_path}/config ]]; then
-		return
-	fi
-
-	echo -e "● Link vdirsyncer configuration file ... \033[1m ${local_data_path}/config\033[0m"
-	ln -sf  ${data_path}/config ${local_data_path}/config
-
-	vdirsyncer discover $discover_module_name
-
-	if [[ $? -ne 0 ]]; then
-		echo -e "\033[31mError: discover module failed ! \033[0m" >&2
+		$common display_error "Config file not found to import !"
 		exit 1
 	fi
 
-	vdirsyncer sync
 
-	if [[ $? -ne 0 ]]; then
-		echo -e "\033[31mError: sync failed ! \033[0m" >&2
-		exit 1
+	$common display_info "link" "Configure vdirsyncer -> ${local_data_path}/config"
+
+	if ! [[ -d $local_data_path ]]; then
+		mkdir -p $local_data_path
 	fi
+
+	sudo ln -sfr ${data_path}/config ${local_data_path}/config || {
+		$common display_error "Configure vdirsyncer failed !"
+		exit 1
+	}
+
+	$common display_info "discover" "vdirsyncer"
+	vdirsyncer discover $discover_module_name || {
+		$common display_error "Discover module failed !"
+		exit 1
+	}
+
+	$common display_info "sync" "vdirsyncer"
+	vdirsyncer sync || {
+		$common display_error "Sync failed !"
+		exit 1
+	}
+
+	$common display_info "installed" "vdirsyncer"
 }
 
 if [[ $OSTYPE != linux-gnu* ]]; then
-	echo -e "\033[31mError: vdirsyncer only for Linux\033[0m"
+	$common display_error "This script is only for linux !"
 	exit 1
 fi
 
