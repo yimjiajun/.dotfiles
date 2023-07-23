49635fa3fbc0190168c38703f2b8ab8cd9c88a77
JiaJun Yim <yimjiajun@icloud.com>
2023-07-23 12:01:38 -0700

    fix data/bash: rename .bash_profile which will run default instead of .bashrc

--- tools/bash.sh
+++ tools/bash.sh
@@ -8,23 +8,23 @@ data_path="$path/../data"
 install() {
 	$common display_title "${tool^^}"
 
-	if [[ -f $data_path/.bash_profile ]]; \
+	if [[ -f $data_path/.bash_setup ]]; \
 	then
 		if [[ -f $HOME/.$(basename $SHELL)rc ]] \
-			&& [[ $(grep -c "source $HOME/.bash_profile" \
+			&& [[ $(grep -c "source $HOME/.bash_${USER}" \
 				$HOME/.$(basename $SHELL)rc) -eq 0 ]]; \
 		then
 			$common display_info "export" \
-				"source $HOME/.bash_profile >> $HOME/.$(basename $SHELL)rc"
+				"source $HOME/.bash_${USER} >> $HOME/.$(basename $SHELL)rc"
 
-			echo "source $HOME/.bash_profile" >> $HOME/.$(basename $SHELL)rc
+			echo "source $HOME/.bash_${USER}" >> $HOME/.$(basename $SHELL)rc
 		fi
 
 		$common display_info "link" \
-			"$HOME/.bash_profile -> $data_path/.bash_profile "
+			"$HOME/.bash_setup -> $data_path/.bash_${USER} "
 
-		ln -sf $data_path/.bash_profile $HOME/.bash_profile || { \
-			$common display_error ".bash_profile link failed"; \
+		ln -sf $data_path/.bash_setup $HOME/.bash_${USER} || { \
+			$common display_error ".bash_setup link failed"; \
 			exit 1; \
 		}
 	fi
