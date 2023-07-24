a11e885a7fbed2fddf8c4da887b3c2380d41067c
JiaJun Yim <yimjiajun@icloud.com>
2023-07-23 18:16:25 -0700

    feat tools/wslu: copy wsl configuration file to window

--- tools/wslu.sh
+++ tools/wslu.sh
@@ -2,6 +2,8 @@
 
 tool='wslu'
 path=$(dirname $(readlink -f $0))
+data_path="$path/../data"
+data_file=".wslconfig"
 common="$path/../app/common.sh"
 install="$path/manual/install_pkg_cmd.sh"
 
@@ -13,7 +15,23 @@ install() {
 		exit 1
 	}
 
+	local usr_path=$(find /mnt/c/Users -maxdepth 1 -type d -not \( \
+			-name 'Default' -o -name 'Public' \
+			-o -name 'Administrator' -o -name 'User' \) \
+			-not -path /mnt/c/Users)
+
+	$common display_info "copy" "conifguration file to \e[1m$usr_path/$data_file\e[0m"
+	dd if=$data_path/$data_file of=$usr_path/$data_file || {
+		$common display_error "copy $tool configuration $data_path/$data_file to $usr_path/$data_file failed"
+		exit 1
+	}
+
 	$common display_info "help" "\033[1;33mwslview\033[0m to open file/url in Windows"
+
+	wslfetch || {
+		$common display_error "unable to run wslu package"
+		exit 1
+	}
 }
 
 if [[ $OSTYPE != linux-gnu* ]]; then
