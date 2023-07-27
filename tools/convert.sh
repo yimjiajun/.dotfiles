db12a735d5766b44e5570928719a784277a43c25
JiaJun Yim <yimjiajun@icloud.com>
2023-07-27 14:38:12 -0700

    feat tools/convert: image convertor

---
+++ tools/convert.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+tool='imagemagick'
+path=$(dirname $(readlink -f $0))
+common="$path/../app/common.sh"
+install="$path/manual/install_pkg_cmd.sh"
+
+install() {
+	$common display_title "Install $tool"
+	$install $tool || {
+		$common display_error "install $tool failed !"
+		exit 1
+	}
+}
+
+if [[ $OSTYPE != linux-gnu* ]]; then
+	$common display_error "not support $tool on $OSTYPE !"
+	exit 1
+fi
+
+if [[ -z "$(which convert)" ]] ||\
+	[[ $1 == "install" ]]; then
+	install
+fi
+
+exit 0
