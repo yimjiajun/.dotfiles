8b2a00c9174e0292622b9374a02a3571a99f1585
JiaJun Yim <yimjiajun@icloud.com>
2023-09-17 14:37:28 -0700

    feat toosl: translate shell
    translate language from Google Transalte
    
    Usage:
    
    Automatic translate to specific language:
    1. Chinese: trans :zh [word]
    2. English: trans :en [word]
        - Chinese + English: trans :zh+en [word]
    
    Translate from specific language:
    1. Chinese: trans zh: 翻譯
    
    Translate a words as one argument:
    1. trans en:zh "word processor"
    
    Translate each word: (without double quotes)
    1. trans en:zh word processor

---
+++ tools/translate.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+tool='translate-shell'
+path=$(dirname $(readlink -f $0))
+common="$path/../app/common.sh"
+
+install() {
+	local tmp_dir="/tmp/translate-shell"
+
+	[ -d $tmp_dir ] && rm -rf $tmp_dir
+
+	git clone --depth 1 https://github.com/soimort/translate-shell $tmp_dir
+
+	 cd $tmp_dir || {
+		$common display_error "change directory to $tmp_dir failed !"
+	}
+
+	make || {
+		$common display_error "make $tool failed !"
+		exit 1
+	}
+
+	sudo make install || {
+		$common display_error "install $tool failed !"
+		exit 1
+	}
+}
+
+if [[ -z "$(which $tool)" ]] ||\
+	[[ $1 == "install" ]]; then
+	install
+fi
+
+exit 0
