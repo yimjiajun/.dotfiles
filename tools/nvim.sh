755b0bb2e6b746af5a68bd7fc73f465bc268375d
JiaJun Yim <yimjiajun@icloud.com>
2023-10-08 17:05:54 -0700

    fix tools/nvim: cargo installed by package without extra setup

--- tools/nvim.sh
+++ tools/nvim.sh
@@ -397,6 +397,8 @@ function pre_install_cargo {
 			echo -e "\033[31mError: Install cargo failed!\033[0m" >&2
 			return 1
 		}
+
+		return 0
 	else
 		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | \
 			sh -s -- --default-toolchain none -y || {
