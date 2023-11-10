ae14871c4e98efa3b49c26149ed4c895596afbdc
JiaJun Yim <yimjiajun@icloud.com>
2023-11-10 00:37:14 -0800

    feat tools/lm_sensors: system sensors information

---
+++ tools/lm_sensors.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+tool='lm-sensors'
+path=$(dirname $(readlink -f $0))
+common="$path/../app/common.sh"
+install="$path/manual/install_pkg_cmd.sh"
+
+install() {
+	$common display_title "Install $tool"
+
+	$install $tool || {
+		$common display_error "install $tool failed !"
+		exit 1
+	}
+
+	tool='sensors'
+
+	if [[ -z "$(which $tool)" ]]; then
+		$common display_error "$tool not found !"
+		exit 1
+	fi
+
+	$tool || {
+		$common display_error "run $tool failed !"
+		exit 1
+	}
+}
+
+if [[ -z "$(which $tool)" ]] ||\
+	[[ $1 == "install" ]]; then
+	install
+fi
+
+exit 0
