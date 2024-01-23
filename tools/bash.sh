48e5333951c9beec41c11d4e4a3b49a211b7abae
JiaJun Yim <yimjiajun@icloud.com>
2024-01-23 10:30:03 +0800

    fix tools/bash: support substituion with bash v3.2

--- tools/bash.sh
+++ tools/bash.sh
@@ -6,7 +6,7 @@ common="$path/../app/common.sh"
 data_path="$path/../data"
 
 install() {
-	$common display_title "${tool^^}"
+	$common display_title "${tool}"
 
 	if [[ -f $data_path/.bash_setup ]]; \
 	then
@@ -23,7 +23,7 @@ install() {
 		$common display_info "link" \
 			"$HOME/.bash_setup -> $data_path/.bash_${USER} "
 
-		ln -sfr $data_path/.bash_setup $HOME/.bash_${USER} || { \
+		ln -sf $data_path/.bash_setup $HOME/.bash_${USER} || { \
 			$common display_error ".bash_setup link failed"; \
 			exit 1; \
 		}
@@ -32,7 +32,7 @@ install() {
 	$common display_info "link" \
 		"$HOME/.bash_aliases -> $data_path/.bash_aliases"
 
-	ln -sfr $data_path/.bash_aliases $HOME/.bash_aliases || { \
+	ln -sf $data_path/.bash_aliases $HOME/.bash_aliases || { \
 		$common display_error ".bash_aliases link failed"; \
 		exit 1; \
 	}
