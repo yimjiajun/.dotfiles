5447630f06fda8b5a365cf64de852dfea8a1d9b0
JiaJun Yim <yimjiajun@icloud.com>
2024-04-18 06:57:58 +0800

    feat tools/zathura: support darwin

--- tools/zathura.sh
+++ tools/zathura.sh
@@ -8,6 +8,13 @@ install="$path/manual/install_pkg_cmd.sh"
 function install() {
 	$common display_title "Install $tool"
 
+	if [[ $OSTYPE == "darwin"* ]]; then
+		brew tap zegervdv/zathura || $common display_error "brew tap the repository failed !"
+		$install zathura-pdf-poppler || $common display_error "brew install zathura pdf poppler failed !"
+		mkdir -p $(brew --prefix zathura)/lib/zathura
+		ln -s $(brew --prefix zathura-pdf-poppler)/libpdf-poppler.dylib $(brew --prefix zathura)/lib/zathura/libpdf-poppler.dylib
+	fi
+
 	$install "$tool" || {
 		$common display_error "install $tool failed !"
 		exit 1
