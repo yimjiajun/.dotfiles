097939172f3f336f765fb0883f375d8abe1483f2
JiaJun Yim <yimjiajun@icloud.com>
2024-04-18 06:56:50 +0800

    feat tools/manual/install_pkg_cmd: support brew installation and progress bar

--- tools/manual/install_pkg_cmd.sh
+++ tools/manual/install_pkg_cmd.sh
@@ -3,7 +3,7 @@
 path=$(dirname $(readlink -f $0))
 common=$path/../../app/common.sh
 
-if [[ $# -eq 0 ]]; then
+if [ $# -eq 0 ]; then
 	$common display_error "Please input pkg name!"
 	exit 1
 fi
@@ -32,13 +32,17 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	fi
 
 elif [[ $OSTYPE == "darwin"* ]]; then
-	if [[ -z $(command -v brew) ]]; then
+	if [ -z $(command -v brew) ]; then
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+		(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ${HOME}/.zprofile
+	fi
+
+	if [ -z $(command -v brew) ]; then
 		$common display_error "brew not install, please install brew first!"
 		exit 1
 	fi
 
 	pkg_install_cmd="brew install"
-
 else
 	$common display_error "kernel-${OSTYPE} Not Support!"
 	exit 1
@@ -47,9 +51,18 @@ fi
 failed=0
 
 for pkg in $@; do
-	$pkg_install_cmd $pkg 1>/dev/null
+	$pkg_install_cmd $pkg 1>/dev/null &
+	pid=$!
 
-	if [[ $? -ne 0 ]]; then
+	while kill -0 $pid 2>/dev/null; do
+		echo -n "."
+		sleep 1
+	done
+	echo ''
+
+	wait $pid
+
+	if [ $? -ne 0 ]; then
 		$common display_error "$pkg failed!"
 		failed=1
 	else
