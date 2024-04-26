2cdd077ac0927deeca3b44b239894316c89b0922
JiaJun Yim <yimjiajun@icloud.com>
2024-04-26 12:55:30 +0800

    refactor: source app/common.sh
    1. merge install package dependencies into common.sh
    2. install package moved into common.sh
    3. remove invokes common.sh

--- app/common.sh
+++ app/common.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 screen_width="$(tput cols)"
+common_working_path="$(dirname $(dirname $(readlink -f $0)))"
+common_data_path="${common_working_path}/data"
+common_local_data_path="${common_working_path}/.localdata"
+common_force_install_param="(install|--force|-f)$"
 
 display_center() {
   local text="$1"
@@ -84,12 +88,82 @@ display_message() {
   echo -e "● $msg"
 }
 
-if [[ $# -lt 2 ]]; then
-  display_error "Usage: $0 <function> <args>"
-  exit 1
-fi
+install_package() {
+    if [ $# -eq 0 ]; then
+        display_error "Please input pkg name!"
+        exit 1
+    fi
 
-func="$1"
-shift
-$func "$@"
-exit
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        if [[ -f /etc/os-release ]]; then
+            . /etc/os-release
+
+            if [[ -n $ID_LIKE ]]; then
+                os=$ID_LIKE
+            else
+                os=$ID
+            fi
+        fi
+
+        if [[ -z $os ]]; then
+            display_error "OS not support!"
+            exit 1
+        fi
+
+        if [[ "$os" == "debian" ]]; then
+            pkg_install_cmd="sudo apt-get install -y"
+        else
+            display_error "OS-${os} Not Support!"
+            exit 1
+        fi
+
+    elif [[ $OSTYPE == "darwin"* ]]; then
+        if [ -z $(command -v brew) ]; then
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            (
+            echo
+            echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+            ) >>${HOME}/.zprofile
+        fi
+
+        if [ -z $(command -v brew) ]; then
+            display_error "brew not install, please install brew first!"
+            exit 1
+        fi
+
+        pkg_install_cmd="brew install"
+    else
+        display_error "kernel-${OSTYPE} Not Support!"
+        exit 1
+    fi
+
+    failed=0
+
+    for pkg in $@; do
+        $pkg_install_cmd $pkg 1>/dev/null &
+        pid=$!
+
+        while kill -0 $pid 2>/dev/null; do
+            echo -n "."
+            sleep 1
+        done
+        echo ''
+
+        wait $pid
+
+        if [ $? -ne 0 ]; then
+            display_error "$pkg failed!"
+            failed=1
+        else
+            display_info "installed" "$pkg"
+        fi
+    done
+
+    exit $failed
+}
+
+if [ $# -ge 2 ]; then
+    func="$1"; shift
+    $func "$@"
+    exit
+fi
