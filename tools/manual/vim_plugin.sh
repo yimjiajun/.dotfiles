00b9a0ed6744968b6058e27e0fa8615ee4b259e8
JiaJun Yim <yimjiajun@icloud.com>
2024-05-19 12:02:33 +0800

    fix tools/manual/vim_plugin: check .cache directory and link the file

--- tools/manual/vim_plugin.sh
+++ tools/manual/vim_plugin.sh
@@ -10,14 +10,14 @@ calendar_crendiatials="$HOME/.cache/calendar.vim/credentials.vim"
 calendar_crendiatials_localdata="$common_local_data_path/calendar.vim/credentials.vim"
 
 if ! [ -f "$calendar_crendiatials" ]; then
-  if [ -d "$(dirname $calendar_crendiatials)" ]; then
+  if ! [ -d "$(dirname $calendar_crendiatials)" ]; then
     mkdir -p "$(dirname $calendar_crendiatials)"
   fi
 
-  display_info "clone" "$calendar_crendiatials"
+  display_info "link" "$calendar_crendiatials"
 
-  if ! cp $calendar_crendiatials_localdata $calendar_crendiatials; then
-    display_error "failed to clone calendar credentials"
+  if ! ln -sfr $calendar_crendiatials_localdata $calendar_crendiatials; then
+    display_error "failed to link calendar credentials"
     exit 1
   fi
 fi
