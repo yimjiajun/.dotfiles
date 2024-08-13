5629221491cfbd9bff53112d4710ec9271992675
JiaJun Yim <yimjiajun@icloud.com>
2024-08-13 14:35:31 +0800

    fix tools/pass: install clipboard for password-store

--- tools/pass.sh
+++ tools/pass.sh
@@ -5,17 +5,27 @@ path="$(dirname $(readlink -f $0))"
 working_path="$(dirname "$path")"
 source "$working_path/app/common.sh"
 
-function install {
-  display_title "Install $tool"
-  if ! install_package $tool; then
-    display_error "install $tool failed !"
-  fi
-}
-
-if [ -z "$(which $tool)" ] || [[ $1 =~ $common_force_install_param ]]; then
-  install
-  display_info "install" "install $tool success"
-  display_info "manual" "Please manual install $working_path/manual/pass.sh"
+if [ -n "$(which $tool)" ] && ! [[ $1 =~ $common_force_install_param ]]; then
+  exit 0
 fi
+
+display_title "Install $tool"
+
+if ! install_package $tool; then
+  display_error "install $tool failed !"
+fi
+
+if [[ $OSTYPE == "linux-gnu" ]]; then
+  . /etc/os-release
+
+  if [ "$ID" == "debian" ] || [ "$ID" == "ubuntu" ]; then
+    if ! install_package xclip wl-clipboard; then
+      display_error "install xclip and wl-clipboard failed !"
+    fi
+  fi
+fi
+
+display_info "install" "install $tool success"
+display_info "manual" "Please manual install $working_path/manual/pass.sh"
 
 exit 0
