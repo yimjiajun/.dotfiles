72666d9c05c4f55e57c7d90bfef138a69b1d87fe
JiaJun Yim <yimjiajun@icloud.com>
2024-08-18 03:47:56 +0800

    feat tools/fzf: add fzf git tools

--- tools/fzf.sh
+++ tools/fzf.sh
@@ -5,17 +5,26 @@ path="$(dirname $(readlink -f $0))"
 working_path="$(dirname "$path")"
 source "$working_path/app/common.sh"
 
-function install {
-  display_title "Install $tool"
+if [ -n "$(which $tool)" ] && ! [[ $1 =~ $common_force_install_param ]]; then
+  exit 0
+fi
 
-  if ! install_package $tool; then
-    display_error "install $tool failed !"
-    exit 1
-  fi
-}
+display_title "Install $tool"
 
-if [ -z "$(which $tool)" ] || [[ $1 =~ $common_force_install_param ]]; then
-  install
+if ! install_package $tool; then
+  display_error "install $tool failed !"
+  exit 1
+fi
+
+display_info "install" "fzf git"
+
+if ! [ -d $HOME/.config/fzf ]; then
+  mkdir -p $HOME/.config/fzf
+fi
+
+if ! ln -sfr ${common_data_path}/.config/fzf/* $HOME/.config/fzf/; then
+  display_error "link fzf config failed !"
+  exit 1
 fi
 
 exit 0
