d92b3df9b2d59e17dd736b306452e3d8e692161a
JiaJun Yim <yimjiajun@icloud.com>
2024-10-05 00:32:05 +0800

    fix formatting: support skip sh format errors

--- formatting.sh
+++ formatting.sh
@@ -23,12 +23,25 @@ for f in $files; do
   fi
 
   if [ "${f##*.}" == 'md' ]; then
-    mdformat $f
-    sed -i 's/[[:space:]]*$//' $f
+    mdformat "$f"
+    sed -i 's/[[:space:]]*$//' "$f"
   fi
 
   if [ "${f##*.}" == 'sh' ]; then
-    shfmt -i 2 -ci -bn -w $f
+    skip_errors=("parameter expansion requires a literal")
+    sh_msg="$(shfmt -i 2 -ci -bn -w "$f" 2>&1)"
+
+    if [ $? -ne 0 ]; then
+      for msg in "${skip_errors[@]}"; do
+        if [ "$(echo "$sh_msg" | grep -c "$msg")" -gt 0 ]; then
+          echo -e "(SKIP) \033[0;33m$sh_msg\033[0m"
+          continue 2
+        fi
+      done
+
+      echo -e "(ERROR) \033[0;31m$sh_msg\033[0m"
+      exit 1
+    fi
   fi
 
   index=$((index + 1))
