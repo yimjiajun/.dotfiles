60c008792fde54c563628dda5355458fda6c14d4
JiaJun Yim <yimjiajun@icloud.com>
2024-10-24 15:57:38 +0800

    feat tools/vifm: vim fila manager with configuration file

--- tools/vifm.sh
+++ tools/vifm.sh
@@ -5,17 +5,33 @@ path="$(dirname $(readlink -f $0))"
 working_path="$(dirname "$path")"
 source "$working_path/app/common.sh"
 
-function install {
-  display_title "Install $tool"
+display_title "Install $tool"
 
-  if ! install_package $tool; then
-    display_error "Failed install $tool"
+if [ -n "$(which $tool)" ] && ! [[ $1 =~ $common_force_install_param ]]; then
+  exit
+fi
+
+if ! install_package $tool; then
+  display_error "Failed install $tool"
+  exit 1
+fi
+
+config_file=".config/vifm/vifmrc"
+common_config_file="${common_data_path}/${config_file}"
+local_config_file="${HOME}/${config_file}"
+
+if ! [ -d "$(dirname ${local_config_file})" ]; then
+  if ! mkdir -p "$(dirname ${local_config_file})"; then
+    display_error "mkdir -p $(dirname ${local_config_file}) failed !"
     exit 1
   fi
-}
-
-if [ -z "$(which $tool)" ] || [[ $1 =~ $common_force_install_param ]]; then
-  install
 fi
+
+if ! ln -sfr "$common_config_file" "$local_config_file"; then
+  display_error "ln -sfr $common_config_file $local_config_file failed !"
+  exit 1
+fi
+
+display_info "link" "$common_config_file -> $local_config_file"
 
 exit 0
